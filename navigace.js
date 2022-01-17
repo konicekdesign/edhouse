6949a68f7c95269fbee6e30a011c9d68d0b6fd8f
konicekdesign <97729731+konicekdesign@users.noreply.github.com>
2022-01-17 18:34:10 +0100

    Update navigace.js

--- navigace.js
+++ navigace.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  vymazID; //vymazani duplicitnich ID
+  vymazID(); //vymazani duplicitnich ID
   //pozice scrollu
   var percent;
     window.addEventListener("scroll", getScrollPercent);
